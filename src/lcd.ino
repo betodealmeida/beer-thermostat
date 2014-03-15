d75d4d4a998b4e7b22eb0edd30a0807d64e4c664
Roberto De Almeida <roberto@dealmeida.net>
2014-03-14 23:44:36 -0700

    Version with Dallas sensor and PID
    This version uses a Dallas temperature sensor and the PID library for a
    better control.

--- src/lcd.ino
+++ src/lcd.ino
@@ -1,100 +1,86 @@
 /*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ Beer thermostat for brewing.
  */
 
-// include the library code:
+#include <PID_v1.h>
+#include <OneWire.h>
 #include <LiquidCrystal.h>
-#include <DHT.h>
+#include <DallasTemperature.h>
 
 // sensor pin
-#define DHTPIN 6
-#define DHTTYPE DHT11 
-DHT dht(DHTPIN, DHTTYPE);
+#define ONE_WIRE_BUS 6
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
-float minTemperature = 7.;
-float maxTemperature = 22.;
-int threshold = 1;
-int targetTemperature;
-int currentTemperature;
-int currentHumidity;
-
-// initialize the library with the numbers of the interface pins
+// LCD init
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-void setup() {
-    dht.begin();
+// PID relay control
+#define RELAY_PIN 7
+double setPoint;  // desired temperature
+double input;     // current temperature
+double output;    // for PID
+PID myPID(&input, &output, &setPoint, 2, 5, 1, REVERSE);
+int windowSize = 5*60*1000;  // 5 minutes in ms
+unsigned long windowStartTime;
 
-    // set up the LCD's number of columns and rows: 
+// pot
+#define POT_PIN A0
+double minTemperature = 7.;
+double maxTemperature = 22.;
+
+void setup() {
     lcd.begin(16, 2);
+    sensors.begin();
+    windowStartTime = millis();
 
     // store initial temperature 
-    targetTemperature = getTemperature(analogRead(A0));
-    currentTemperature = int(dht.readTemperature());
-    currentHumidity = int(dht.readHumidity());
+    setPoint = getSetPoint();
+    input = getInput();
+
+    // set PID
+    myPID.SetOutputLimits(0, windowSize);
+    myPID.SetMode(AUTOMATIC);
+
+    // draw LCD
     draw();
 }
 
 void loop() {
-    int temperature, humidity;
+    double temp;
     bool redraw = false;
 
     // check target temperature
-    temperature = getTemperature(analogRead(A0));
-    if (temperature != targetTemperature) {
-        targetTemperature = temperature;
+    temp = getSetPoint();
+    if (temp != setPoint) {
+        setPoint = temp;
         redraw = true;
     }
 
     // check current temperature
-    temperature = int(dht.readTemperature());
-    if (temperature != currentTemperature) {
-        currentTemperature = temperature;
-        redraw = true;
-    }
-
-    // check current humidity;
-    humidity = int(dht.readHumidity());
-    if (humidity != currentHumidity) {
-        currentHumidity = humidity;
+    temp = getInput();
+    if (temp != input) {
+        input = temp;
         redraw = true;
     }
 
     if (redraw) draw();
 
-    // turn on/off
+    // do PID
+    myPID.Compute();
+    unsigned long now = millis();
+    if (now - windowStartTime > windowSize) {
+        windowStartTime += windowSize;
+    }
+    if (output > now - windowStartTime) {
+        //digitalWrite(RelayPin,HIGH);
+        lcd.setCursor(15, 0);
+        lcd.print("*");
+    } else {
+        //digitalWrite(RelayPin,LOW);
+        lcd.setCursor(15, 0);
+        lcd.print(" ");
+    }
 }
 
 void draw() {
@@ -104,33 +90,35 @@ void draw() {
     lcd.setCursor(0, 0);
     lcd.print("now:");
     lcd.setCursor(5, 0);
-    lcd.print(currentTemperature, DEC);
+    lcd.print(int(input), DEC);
     lcd.print("C");
-
-    // current humidity
-    lcd.print(" / ");
-    lcd.print(currentHumidity, DEC);
-    lcd.print("%");
 
     // target temperature
     lcd.setCursor(0, 1);
     lcd.print("set:");
     lcd.setCursor(5, 1);
-    lcd.print(targetTemperature, DEC);
+    lcd.print(int(setPoint), DEC);
     lcd.print("C");
 
     // append "ale" or "lager"
-    if ((targetTemperature >= 7) && (targetTemperature <= 9)) {
+    if ((setPoint >= 7) && (setPoint <= 9)) {
         lcd.print("  (lager)");
-    } else if ((targetTemperature >= 10) && (targetTemperature <= 13)) {
+    } else if ((setPoint >= 10) && (setPoint < 13+1)) {
         lcd.print(" (lager)");
-    } else if ((targetTemperature >= 20) && (targetTemperature <= 22)) {
+    } else if ((setPoint >= 20) && (setPoint < 22+1)) {
         lcd.print(" (ale)");
     }
 }
 
-int getTemperature(float value) {
-    float offset = minTemperature;
-    float delta = 1 + maxTemperature - minTemperature;
-    return int(offset + delta * value / 1024.);
+double getSetPoint() {
+    double value = analogRead(POT_PIN);
+    double offset = minTemperature;
+    double delta = 1 + maxTemperature - minTemperature;
+    return offset + delta * value / 1024.;
 }
+
+double getInput() {
+    sensors.requestTemperatures();
+    return sensors.getTempCByIndex(0);
+}
+
